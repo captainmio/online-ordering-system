21c319157d6ca3ff9f9041ff1248c055517e5697
Rustum Jordan <rustum@thinksmartsoftware.com>
2022-05-18 23:17:35 +0800

    additional update

--- frontend/src/components/InputTextfield.vue
+++ frontend/src/components/InputTextfield.vue
@@ -1,11 +1,20 @@
 <template>
-  <q-input outlined :type="type" v-model="text" :label="label" />
+  <q-input
+    outlined
+    :type="type"
+    :label="label"
+    @change="$emit('update:modelValue', $event.target.value)"
+  />
 </template>
 
 <script>
 export default {
   name: "InputTextfield",
-  props: ["type", "label"],
+  props: {
+    type: String,
+    label: String,
+    modelValue: String,
+  }[("type", "label")],
 };
 </script>
 
